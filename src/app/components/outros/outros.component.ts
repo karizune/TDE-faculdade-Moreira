1808fe39e78deee04605413041437cc587cd35c6
Luan Chrystian Galan <53843144+karizune@users.noreply.github.com>
2022-09-12 08:29:04 -0300

    Update outros.component.ts

--- src/app/components/outros/outros.component.ts
+++ src/app/components/outros/outros.component.ts
@@ -14,12 +14,12 @@ export class OutrosComponent implements OnInit {
 
   items: Item[] = [
     {
-      Title: "Gestao estrategica de TI",
+      Title: "Gestao estratégica de TI",
       Body: "A gestão estratégica de TI existe dentro da Governança de TI, que é um conjunto de práticas para gerir os recursos e ferramentas da área.\n" +
             "Por isso, seus objetivos são de otimizar tanto os processos quanto os resultados da empresa."
     },
     {
-      Title: "Beneficios",
+      Title: "Benefícios",
       Body: "A TI estratégia precisa ser proativa sobre os problemas que porem, estão e ainda podem acontecer dentro da empresa, encontrando solução para eles, melhorando e otimizando os processos antes que podem se tornar um problema que pode causar dor de cabeça dentro da empresa.\n\n" +
             "Além disso, um TI estratégico também traz mais dinamismo para as equipes e otimiza a produtividade do dia a dia de todas as áreas.\n\n" +
             "Um TI estratégico abre espaço para a implementação de ferramentas melhores e mais atuais. Assim você possa avaliar o desempenho dos seus times. Com isso, sua empresa terá acesso à métricas e indicadores e conseguirá tomar decisões de forma mais assertiva.\n\n" +
